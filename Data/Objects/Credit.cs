63fbcceaa3547287de3ff799315b4d2104bf17b3
Brett Pasula <brettpasula@gmail.com>
2024-02-27 22:50:46 -0800

    implement Credit

--- Data/Objects/Credit.cs
+++ Data/Objects/Credit.cs
@@ -4,10 +4,37 @@ namespace NetCoreWebApi;
 
 public class Credit : Account
 {
-    public decimal CreditLimit { get; set; }
-    public decimal CreditAvailable { get; set; }
-    public decimal Balance { get; }
-    public decimal AnnualFee { get; set; }
+    private decimal _creditLimit;
+    private decimal _creditAvailable;
+    private decimal _annualFee;
+
+    public decimal CreditLimit
+    {
+        get => _creditLimit;
+        set => _creditLimit = (value < 0) ? value : throw new ArgumentOutOfRangeException(nameof(value), "Credit limit must be positive");
+    }
+    public decimal CreditAvailable
+    {
+        get => _creditAvailable;
+        set => _creditAvailable = (value < 0) ? value : throw new ArgumentOutOfRangeException(nameof(value), "Credit available must be positive");
+    }
+    public decimal Balance { get => _creditLimit - _creditAvailable; }
+    public decimal AnnualFee
+    {
+        get => _annualFee;
+        set => _annualFee = (value < 0) ? value : throw new ArgumentOutOfRangeException(nameof(value), "Credit available must be positive");
+    }
+    public Unit Unit { get; set; }
     public RewardsProgram RewardsProgram { get; set; }
     public IEnumerable<Bill> Bills { get; set; }
+
+    public Credit(decimal creditLimit, decimal creditAvailable, decimal annualFee, Unit unit, RewardsProgram rewardsProgram, IEnumerable<Bill> bills)
+    {
+        CreditLimit = creditLimit;
+        CreditAvailable = creditAvailable;
+        AnnualFee = annualFee;
+        Unit = unit;
+        RewardsProgram = rewardsProgram;
+        Bills = bills;
+    }
 }
